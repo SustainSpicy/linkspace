323313acaecd63cc2bddf907ac55d4748fb07404
sustainspicy <isrealspicy@gmail.com>
2024-01-16 15:14:07 +0100

    feature: profile update api

--- api/routes/profileRoute.js
+++ api/routes/profileRoute.js
@@ -19,7 +19,7 @@ router.get("/profiles/:id", async (req, res) => {
   try {
     const profile = await PROFILE.findById(id);
     if (!profile) {
-      return res.status(404).json({ error: "Profile not found" });
+      return res.status(404).json({ msg: "Unauthorized action" });
     }
     res.status(200).json(profile);
   } catch (error) {
@@ -28,5 +28,23 @@ router.get("/profiles/:id", async (req, res) => {
   }
 });
 
-router.put();
+router.put("/profiles/:id", async (req, res) => {
+  const updateData = req.body;
+  const { id } = req.params;
+
+  try {
+    //check if profile exists
+    const profile = await PROFILE.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!profile) {
+      return res.status(404).json({ error: "Profile not found" });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+});
 router.delete();
