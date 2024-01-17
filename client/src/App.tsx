67b07e92ab3d15488859a3a989664ed33182fa7d
sustainspicy <isrealspicy@gmail.com>
2024-01-17 17:00:54 +0100

    feature navbar implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,16 @@
-import { useState } from "react";
-import CardWrapper from "./components/CardWrapper";
-import GoogleAuth from "./components/GoogleAuth";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-// import { useGoogleAuth } from "./provider/GoogleAuthProvider";
-import { motion, AnimatePresence } from "framer-motion";
+import Nav from "./components/Nav";
+import Auth from "./pages/Auth";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 
-interface AuthFormProps {
-  type: number;
-}
 function App() {
-  // const { logout } = useGoogleAuth();
-  const [authType, setAuthType] = useState(1);
-
-  const AuthForm = ({ type }: AuthFormProps) => {
-    return (
-      <AnimatePresence>
-        {type === 0 ? <Signup /> : type === 1 ? <Login /> : <></>}
-        <span className="text-gray-600 text-sm">
-          {type === 0 ? "Already have an account? " : "Don't have an account? "}
-
-          <a
-            className="text-blue-500 hover:underline cursor-pointer"
-            onClick={() => setAuthType(type === 0 ? 1 : 0)}
-          >
-            Register
-          </a>
-        </span>
-      </AnimatePresence>
-    );
-  };
-
   return (
-    <motion.div layout>
-      <CardWrapper>
-        <AuthForm type={authType} />
-      </CardWrapper>
-      <div className="socialAuth pt-8 w-full flex justify-center gap-2">
-        <GoogleAuth />
-      </div>
-      {/* <button type="button" onClick={() => logout()}>
-        Logout
-      </button> */}
-    </motion.div>
+    <Router>
+      <Nav />
+
+      <Routes>
+        <Route path="/signin" element={<Auth />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
